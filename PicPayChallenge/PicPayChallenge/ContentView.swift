8022f390cd786d2a8f82e855e9da2b2387e593d0
KacioReis <khcb@academy.cin.ufpe.br>
2019-10-17 10:48:36 -0300

    init project in swift UI

---
+++ PicPayChallenge/PicPayChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicPayChallenge
+//
+//  Created by Kacio on 17/10/19.
+//  Copyright © 2019 Kacio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Whjhjbghugguorld")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
